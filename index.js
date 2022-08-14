aff3b01d91911d470fe4c73c60367d35695a90cd
Blobadoodle <me@bloba.dev>
2022-08-14 13:41:25 +0100

    Basic server setup

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const helmet = require('helmet');
+const session = require('express-session');
+const morgan = require('morgan');
+
+const port = process.env.PORT | 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use(helmet());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 24 * 60 * 60 * 1000} // 1 Day
+}));
+app.use(morgan('dev'));
+app.use('/', require('./routes/main.js'));
+app.use('/admin', require('./routes/admin.js'));
+app.set('view engine', 'ejs');
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
