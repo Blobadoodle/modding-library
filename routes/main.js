6b1a139cbb562423d34d4afc08bcf9dcc57a91be
Blobadoodle <me@bloba.dev>
2022-08-17 21:16:30 +0100

    split homepage phones into partials

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('index'); 
+    res.render('index', {phones: [ {name: 'Realme', phones: [{name: 'Realme 7', image: '/images/realme7.png', _id: 'e621'}] } ]}); 
 });
 
 router.get('/phone/e621', (req, res) => {
