bba1c5d0a1526deb57eff7571c35d1800d2b9a71
Blobadoodle <me@bloba.dev>
2022-08-19 11:48:51 +0100

    authenticate routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const passport = require('passport');
 
-router.get('/', (req, res) => {
+router.get('/', passport.authenticate('session', { failureRedirect: '/login' }), (req, res) => {
     res.render('admin/index'); 
 });
 
-router.get('/newpost', (req, res) => {
+router.get('/newpost', passport.authenticate('session', { failureRedirect: '/login' }), (req, res) => {
     res.render('admin/newpost'); 
 });
 
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-router.post('/login', passport.authenticate('local', { failureRedirect: '/login', successRedirect: '/admin'}), (req, res) => {
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
     res.redirect('/admin');
+});
+
+router.post('/phone', passport.authenticate('session', { failureRedirect: '/login' }), (req, res) => {
+    console.log(req.body);
+    res.redirect('/admin/newpost');
 });
 
 module.exports = router;
