71dd4774009c6c3f096da3bd3a9b701d2d9cf8e5
Blobadoodle <me@bloba.dev>
2022-08-19 11:47:24 +0100

    Fix typo

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, index: { unique: true}},
-    password: {type: String, rquired: true}
+    password: {type: String, required: true}
 });
 
 module.exports = new mongoose.model('User', UserSchema);
