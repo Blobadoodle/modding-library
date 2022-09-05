02f2a0e28afe8c19c3c43b4a970f8546ec36b6b5
Blobadoodle <me@bloba.dev>
2022-09-05 21:28:07 +0100

    fix api auth

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
 const passport = require('passport');
 
+// Non-priviliged gateways
+
 router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
     res.redirect('/admin');
 });
+
+router.all('*', (req, res, next) => {
+    if(req.isAuthenticated()) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+});
+
+// Privileged gateways
 
 router.post('/phone', passport.authenticate('session', { failureRedirect: '/login' }), (req, res) => {
     console.log(req.body);
