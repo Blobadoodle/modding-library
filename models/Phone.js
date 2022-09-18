447dfdf5038674c36acf9ec04b43e5df0a21c4c4
Blobadoodle <me@bloba.dev>
2022-09-18 22:05:46 +0100

    phone schema changes

--- models/Phone.js
+++ models/Phone.js
@@ -5,15 +5,15 @@ const PhoneSchema = new mongoose.Schema({
     name: {type: String, required: true},
     model: {type: String, required: true},
     chipset: {type: String, required: true},
-    sdcard: {type: String, required: true},
+    sdcard: {type: String, required: false},
     storage: {type: String, required: true},
     usb: {type: String, required: true},
     battery: {type: String, required: true},
     display: {type: String, required: true},
     os: {type: String, required: true},
     image: {type: String, required: true},
-    sections: {type: Array, required: true},
-    partmap: {type: String, required: true},
+    sections: {type: Object, required: false},
+    partmap: {type: String, required: false},
     addedby: {type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
