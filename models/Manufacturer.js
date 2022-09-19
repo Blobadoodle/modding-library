6f236117593de4ee537388952f846305b7820b88
Blobadoodle <me@bloba.dev>
2022-09-19 14:16:40 +0100

    show database phones on page and index

--- models/Manufacturer.js
+++ models/Manufacturer.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose');
 
 const ManufacturerSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    phones: [{
-        type: mongoose.Schema.Types.ObjectId, ref: 'Phone'    
-    }],
     addedby: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 });
 
--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
+const Phone = require('../models/Phone.js');
+const Manufacturer = require('../models/Manufacturer.js');
 
-router.get('/', (req, res) => {
-    res.render('index', {phones: [ {name: 'Realme', phones: [{name: 'Realme 7', image: '/images/realme7.png', _id: 'e621'}] } ], user: req.user?.username}); 
+router.get('/', async (req, res) => {
+    const phonesObj = [];
+
+    const brands = await Manufacturer.find({});
+    for(const brand of brands) {
+        const obj = {};
+        obj.name = brand.name;
+        const phones = await Phone.find({brand: brand._id});
+        obj.phones = phones;
+        phonesObj.push(obj);
+    }
+
+    res.render('index', {phones: phonesObj, user: req.user?.username}); 
 });
 
-router.get('/phone/e621', (req, res) => {
-    res.render('phone', {user: req.user?.username});
+router.get('/phone/:name', async (req, res) => {
+    const phone = await Phone.find({name: decodeURIComponent(req.params.name)});
+    if(!phone) return res.status(404);
+    console.log(phone);
+    res.render('phone', {user: req.user?.username, phone: phone[0]});
 });
 
 router.get('/login', (req, res) => {
